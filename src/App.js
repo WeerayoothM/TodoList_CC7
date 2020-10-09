b38b55306d42ae56fc252936b1c0a57ac56d7477
Weerayooth Manawanich <weerayooth.ma@gmail.com>
2020-10-09 14:23:24 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./sort-alpha-down-solid.svg";
+import "./App.css";
+import TodoItem from "./components/TodoItem";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    contents: [],
+  };
+  addTodo = () => {
+    let contentState = [...this.state.contents];
+    let addTodoText = prompt("Enter a todo");
+    if (addTodoText === null || addTodoText === "") return;
+    contentState.push(addTodoText);
+
+    this.setState({ contents: contentState });
+  };
+  deleteTodo = (content) => {
+    let contentState = [...this.state.contents];
+    let index = contentState.indexOf(content);
+    contentState.splice(index, 1);
+    this.setState({ contents: contentState });
+  };
+  editTodo = (content, newContent) => {
+    // let contentState = [...this.state.contents];
+    // let index = contentState.indexOf(content);
+    // let editText = prompt("Enter edit todo");
+    // contentState[index] = editText;
+    // this.setState({ contents: contentState });
+    let contentState = [...this.state.contents];
+    let index = contentState.indexOf(content);
+    // let editText = prompt("Enter edit todo");
+    contentState[index] = newContent;
+    this.setState({ contents: contentState });
+  };
+  sortTodo = () => {
+    let contentState = [...this.state.contents];
+    contentState = contentState.sort();
+    this.setState({ contents: contentState });
+  };
+  render() {
+    console.log(this.state.contents);
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>To-Do</h1>
+          <button onClick={() => this.sortTodo()} className="btn-sort">
+            <img src={logo} className="App-logo" alt="logo" />
+            &nbsp; Sort
+          </button>
+        </header>
+        <section className="App-content">
+          <ul className="todo-container">
+            {/* <TodoItem
+              content={"Do homework"}
+              deleteTodo={this.deleteTodo}
+              editTodo={this.editTodo}
+            /> */}
+            {this.state.contents.map((content, idx) => (
+              <TodoItem
+                key={idx}
+                content={content}
+                deleteTodo={this.deleteTodo}
+                editTodo={this.editTodo}
+              />
+            ))}
+            {/* <div className="todo-container">  ร่าง
+            <li className="todotext">helo</li>
+            <button className="btn-edit">Edit</button>
+          </div> */}
+          </ul>
+        </section>
+        <footer className="App-footer">
+          <button onClick={() => this.addTodo()} className="btn-add">
+            + Add a to-do
+          </button>
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default App;
